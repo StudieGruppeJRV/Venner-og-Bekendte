107315c988ddd4536ea5ef155457cff35e946d16
Rimasto <ole.adam.moeller@gmail.com>
2020-10-20 20:54:51 +0200

    Faderen tester

--- presentation/CompareGUI.java
+++ presentation/CompareGUI.java
@@ -50,4 +50,8 @@ public class CompareGUI {
 		MenuGUI back = new MenuGUI();
 		back.start(primaryStage, vob);
 	}
+	private void backTest() {
+		MenuGUI back = new MenuGUI();
+		back.start(primaryStage, vob);
+	}
 }
